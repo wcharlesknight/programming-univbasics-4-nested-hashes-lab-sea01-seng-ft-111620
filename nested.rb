c3b6c43100c58e2779c9decf02dcf1d767d7d68b
Charlie Knight <williamchknight@gmail.com>
2020-11-06 16:42:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,11 +59,8 @@ programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-  :yukihiro_matsumoto => {
-   :known_for => "Ruby",
-   :languages => ["LISP", "C"]
- }
+# add the following information to the top level of 
+
 # return the entire updated hash
 
 	programmer_hash = 
