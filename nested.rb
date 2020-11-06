7187f842d3342997e5392707247dde46dcec9660
Charlie Knight <williamchknight@gmail.com>
2020-11-06 16:42:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -19,7 +19,7 @@ programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+  
 	
 	programmer_hash = 
  		{
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,19 +55,22 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#   :languages => ["LISP", "C"]
-# }
+  :yukihiro_matsumoto => {
+   :known_for => "Ruby",
+   :languages => ["LISP", "C"]
+ }
 # return the entire updated hash
 
 	programmer_hash = 
- 		{
+ 		{  :yukihiro_matsumoto => {
+   :known_for => "Ruby",
+   :languages => ["LISP", "C"]
+ }
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -81,7 +84,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash
 end
 
 def changing_alan
